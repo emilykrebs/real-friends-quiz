6298a9f7771753d48a93ed7a3c4ccef12ee8f86c
Eric Peng <tzerpeng@gmail.com>
2021-01-16 11:38:35 -0500

    update register and login backend

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ const PORT = process.env.PORT;
 const mongoose = require('mongoose');
 
 const surveyRouter = require('./routes/surveyRouter');
+const userRouter = require('./routes/userRouter');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// establish connection with mongoDB --->
 const MONGO_URI = 'mongodb+srv://mole:mole@friends-scratch-project.5ohhn.mongodb.net/friends?retryWrites=true&w=majority';
 mongoose.connect( MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once("open", () => console.log("connected to database"));
@@ -23,10 +25,18 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-// set up route to survey router
+// route requests to /survey endpoint to survey router --->
 app.use('/survey', surveyRouter);
 
+// route requests to /user endpoint to user router --->
+app.use('/user', userRouter);
 
+// general 404 handler for any other endpoints --->
+app.use('*', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// global error handler --->
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
