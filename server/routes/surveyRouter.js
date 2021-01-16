05b9c5d59de726e331f9df11f53e0659c77a6456
Eric Peng <tzerpeng@gmail.com>
2021-01-16 13:41:32 -0500

    finish cookie and isLogin function

--- server/routes/surveyRouter.js
+++ server/routes/surveyRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const surveyController = require('../controller/surveyController')
+const surveyController = require('../controller/surveyController');
+const sessionController = require('../controller/sessionController');
 
 // post /addsurvey
-router.post('/addsurvey', surveyController.createNewSurvey, (req, res) => {
+router.post('/addsurvey', sessionController.isLoggedIn, surveyController.createNewSurvey, (req, res) => {
     res.status(200).json(res.locals.roomId);
 });
 
