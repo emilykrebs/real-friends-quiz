691d616b257c9b05c01a11584c7ab0409fcb4628
Emily Krebs <erkrebs@gmail.com>
2021-01-18 19:04:29 -0500

    merged files

--- server/controller/sessionController.js
+++ server/controller/sessionController.js
@@ -10,13 +10,7 @@ exports.isLoggedIn = (req, res, next) => {
   Session.findOne({ cookieId: ssid })
    .then(data => {
     console.log('COOKIE FOUND----->', data);
-<<<<<<< HEAD
     return data ? (res.locals.verify = true, next()) : (res.locals.verify = false, next());
-=======
-    // check if null is returned -> if so, redirect to login
-    // otherwise, next;
-    return data ? (res.locals.verify = data.cookieId, next()) : (res.locals.verify = false, next());
->>>>>>> 71b95e84a5f097f66d1d41d8c699f68b03ff33d5
   })
   .catch(err => {
     res.locals.verify = false;
